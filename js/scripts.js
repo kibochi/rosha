878b74c5ba047567af68d268040b6de8a8c3ea51
kibochi <kibochibryn@gmail.com>
2022-06-25 18:02:20 +0300

    jj

--- js/scripts.js
+++ js/scripts.js
@@ -146,12 +146,15 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/61fc98d7b9e4e21181bd6433/1g5rno9t3';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
+    // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    // (function(){
+    // var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    // s1.async=true;
+    // s1.src='https://embed.tawk.to/61fc98d7b9e4e21181bd6433/1g5rno9t3';
+    // s1.charset='UTF-8';
+    // s1.setAttribute('crossorigin','*');
+    // s0.parentNode.insertBefore(s1,s0);
+    // })();
+
+      AOS.init();
+   
